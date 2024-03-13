a4ec17271a0e91863dcc5ce456dad72deecaef92
And <And@DESKTOP-1IMSA14>
2024-03-13 20:45:45 -0300

    Add project files.

---
+++ CloudStorageApplication/UseCases/Users/UploadProfilePhoto/UploadProfilePhotoUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CloudStorageApplication.UseCases.Users.UploadProfilePhoto;
+public class UploadProfilePhotoUseCase
+{
+    public void Execute(IFormFile file)
+    {
+        
+    }
+}
---
+++ CloudStorageService/Controllers/StorageController.cs
@@ -0,0 +1,20 @@
+﻿using CloudStorageApplication.UseCases.Users.UploadProfilePhoto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudStorageService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StorageController : ControllerBase
+{
+	[HttpPost]
+	public IActionResult UploadImage(IFormFile file)
+	{
+		var useCase = new UploadProfilePhotoUseCase();
+
+		useCase.Execute(file);
+
+		return Created();
+	}
+}
+ 
